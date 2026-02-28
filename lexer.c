47bd5c491b468ebfd95c7878472d1749e36bfdec
Aadit Malhotra <f20221135@pilani.bits-pilani.ac.in>
2026-02-28 12:38:58 +0530

    Need some return statements for now, and some retraction errors

--- lexer.c
+++ lexer.c
@@ -180,6 +180,13 @@ void increaseBuffer(buffer* twinBuffer){
     twinBuffer->fwdPtr = (twinBuffer->fwdPtr + 1) % (2 * BUFFER_SIZE);
 }
 
+char getNextChar(int index, buffer* buffer) {
+    if(twin_buffer -> eof != -1 && index == twin_buffer -> fwdPtr > twin_buffer -> eof) {
+        return '\0';
+    return (index < BUFFER_SIZE) ? twin_buffer->buf1[index]  : twin_buffer->buf2[index - BUFFER_SIZE];
+    }
+}
+
 void doubleRetract(buffer* twinBuffer){
     if(twinBuffer == NULL) return;
 
@@ -200,6 +207,431 @@ token* createToken(vocab v, char* lexeme, int linenumber) {
     tok->lexeme[MAX_LEX_LENGTH - 1] = '\0'; // added null termination -> may not be needed?
 
     return tok;
+}
+
+token* getNextToken(buffer* twinBuffer) {
+    char c;
+    char lexeme[MAX_LEX_LENGTH];
+    int lexLen = 0;
+
+    while(1) {
+        c = getNextChar(twinBuffer->fwdPtr, twinBuffer);
+
+        switch(dfaState) {
+            case 0: //Start state
+
+                if (c == '\0' || c == EOF) {
+                    return NULL;
+                }
+
+                else if(c == ' ' || c == '\t' || c == '\n') {
+                    increaseBuffer(twinBuffer);
+                    twinBuffer->currPtr = twinBuffer->fwdPtr;
+                }
+
+                else if (c == '\n') {
+                    currLineNum++;
+                    increaseBuffer(twinBuffer);
+                    twinBuffer->currPtr = twinBuffer->fwdPtr;
+                }
+
+                else if(c == '<') {
+                    increaseBuffer(twinBuffer);
+                    dfaState = 1;
+                }
+                else if(c == '&') {
+                    increaseBuffer(twinBuffer);
+                    dfaState = 7;
+                }
+                else if(c == '@') {
+                    increaseBuffer(twinBuffer);
+                    dfaState = 10;
+                }
+                else if(c == '+' || c == '*' || c == '-' || c == '/' || c == '~' || c == ',' || c == ';' || c == ':' || c == '[' || c == ']' || c == '(' || c == ')') {
+                    dfaState = 13;
+                }
+                else if (c == '!') {
+                    increaseBuffer(twinBuffer);
+                    dfaState = 14;
+                }
+                else if (c >= 'b' && c <= 'd') {
+                    increaseBuffer(twinBuffer);
+                    dfaState = 16;
+                }
+                else if (c >= 'a' && c <= 'z') {
+                    increaseBuffer(twinBuffer);
+                    dfaState = 20;
+                }
+                else if (c >= '0' && c <= '9') {
+                    increaseBuffer(twinBuffer);
+                    dfaState = 22;
+                }
+                else if (c == '#') {
+                    increaseBuffer(twinBuffer);
+                    dfaState = 31;
+                }
+                else if (c == '_') {
+                    increaseBuffer(twinBuffer);
+                    dfaState = 34;
+                }
+                else if (c == '>') {
+                    increaseBuffer(twinBuffer);
+                    dfaState = 38;
+                }
+
+                else {
+                    increaseBuffer(twinBuffer);
+                    twinBuffer->currPtr = twinBuffer->fwdPtr;
+                }
+                break;
+
+            case 1: // read '<'
+                if(c == '=') {
+                    increaseBuffer(twinBuffer);
+                    dfaState = 2;
+                }
+
+                else if(c == '-') {
+                    increaseBuffer(twinBuffer);
+                    dfaState = 4;
+                }
+
+                else {
+                    retract(twinBuffer);
+                    dfaState = 3;
+                }
+                break;
+                
+            case 2: // read '<='
+                dfaState = 0;
+                twinBuffer->currPtr = twinBuffer->fwdPtr;
+                return createToken(TK_LE, "<=", currLineNum);
+
+            case 3: // read other
+                dfaState = 0;
+                twinBuffer->currPtr = twinBuffer->fwdPtr;
+                return createToken(TK_LT, "<", currLineNum);
+            
+            case 4: // read '<-'
+                if(c == '-') {
+                    increaseBuffer(twinBuffer);
+                    dfaState = 5;
+                }
+                //double retraction to be implemented
+                break;
+            
+            case 5: // read '<--'
+                if(c == '-') {
+                    increaseBuffer(twinBuffer);
+                    dfaState = 6;
+                }
+                // incomplete error
+                break;
+            
+            case 6: // read '<---'
+                dfaState = 0;
+                twinBuffer->currPtr = twinBuffer->fwdPtr;
+                return createToken(TK_ASSIGNOP, "<--", currLineNum);
+                
+            case 7: // read '&'
+                if(c == '&') {
+                    increaseBuffer(twinBuffer);
+                    dfaState = 8;
+                }
+                // unknown error
+                break;
+                
+            case 8: // read '&&'
+                if(c == '&') {
+                    increaseBuffer(twinBuffer);
+                    dfaState = 9;
+                }
+                // unknown error
+                break;
+                
+            case 9: // read '&&&'
+                dfaState = 0;
+                twinBuffer->currPtr = twinBuffer->fwdPtr;
+                return createToken(TK_AND, "&&&", currLineNum);
+
+            case 10: // read '@'
+                if(c == '@') {
+                    increaseBuffer(twinBuffer);
+                    dfaState = 11;
+                }
+                // unknown error
+                break;
+            
+            case 11: // read '@@'
+                if(c == '@') {
+                    increaseBuffer(twinBuffer);
+                    dfaState = 12;
+                }
+                // unknown error
+                break;
+
+            case 12: // read '@@@'
+                dfaState = 0;
+                twinBuffer->currPtr = twinBuffer->fwdPtr;
+                return createToken(TK_ASSIGNOP, "@@@", currLineNum);
+                break;
+            
+            case 13:
+                increaseBuffer(twinBuffer);
+                dfaState = 0;
+                twinBuffer->currPtr = twinBuffer->fwdPtr;
+                switch(c) {
+                    case '+': return createToken(TK_PLUS, "+", currLineNum);
+                    case '-': return createToken(TK_MINUS, "-", currLineNum);
+                    case '*': return createToken(TK_MUL, "*", currLineNum);
+                    case '/': return createToken(TK_DIV, "/", currLineNum);
+                    case '~': return createToken(TK_NOT, "~", currLineNum);
+                    case ',': return createToken(TK_COMMA, ",", currLineNum);
+                    case ';': return createToken(TK_SEM, ";", currLineNum);
+                    case ':': return createToken(TK_COLON, ":", currLineNum);
+                    case '[': return createToken(TK_SQL, "[", currLineNum);
+                    case ']': return createToken(TK_SQR, "]", currLineNum);
+                    case '(': return createToken(TK_OP, "(", currLineNum);
+                    case ')': return createToken(TK_CL, ")", currLineNum);
+                    default: return NULL;
+                }
+            
+            case 14: // read '!'
+                if(c == '=') {
+                    increaseBuffer(twinBuffer);
+                    dfaState = 15;
+                }
+                // unknown error
+                break;
+
+            case 15: // read '!='
+                dfaState = 0;
+                twinBuffer->currPtr = twinBuffer->fwdPtr;
+                return createToken(TK_NE, "!=", currLineNum);
+
+            case 16: // read b-d
+                if(c >= 'a' && c <= 'z') {
+                    increaseBuffer(twinBuffer);
+                    dfaState = 20;
+                }
+
+                else if (c >= '2' && c <= '7') {
+                    increaseBuffer(twinBuffer);
+                    dfaState = 17;
+                }
+
+                break;
+            
+            case 17: // read b-d followed by 2-7
+                if (c >= 'b' && c <= 'd') {
+                    increaseBuffer(twinBuffer);
+                    dfaState = 17;
+                }
+
+                else if (c >= '2' && c <= '7') {
+                    increaseBuffer(twinBuffer);
+                    dfaState = 18;
+                }
+
+                else {
+                    retract(twinBuffer);
+                    dfaState = 19;
+                }
+                break;
+            
+            case 18: // read b-d followed by 2-7 followed by b-d or 2-7
+                if(c >= '2' && c <= '7') {
+                    increaseBuffer(twinBuffer);
+                    dfaState = 18;
+                }
+
+                else {
+                    retract(twinBuffer);
+                    dfaState = 19;
+                }
+            
+            case 19: // read other
+                dfaState = 0;
+                twinBuffer->currPtr = twinBuffer->fwdPtr;
+                return createToken(TK_ID, lexeme, currLineNum);
+            
+            case 20: // read a-z
+                if(c >= 'a' && c <= 'z') {
+                    increaseBuffer(twinBuffer);
+                    dfaState = 20;
+                }
+
+                else {
+                    retract(twinBuffer);
+                    dfaState = 21;
+                }
+                break;
+            
+            case 21: // read other
+                dfaState = 0;
+                twinBuffer->currPtr = twinBuffer->fwdPtr;
+                return createToken(TK_FIELDID, lexeme, currLineNum);
+
+            case 22: // read 0-9
+                if(c >= '0' && c <= '9') {
+                    increaseBuffer(twinBuffer);
+                    dfaState = 22;
+                }
+                
+                else if (c == '.') {
+                    increaseBuffer(twinBuffer);
+                    dfaState = 23;
+                }
+
+                else {
+                    retract(twinBuffer);
+                    dfaState = 30;
+                }
+                break;
+
+            case 23: // read 0-9 followed by '.'
+                if(c >= '0' && c <= '9') {
+                    increaseBuffer(twinBuffer);
+                    dfaState = 24;
+                }
+                break;
+
+            case 24: // read 0-9 followed by '.' followed by 0-9
+                if (c >= '0' && c <= '9') {
+                    increaseBuffer(twinBuffer);
+                    dfaState = 24;
+                }
+                break;
+            
+            case 25: // read 0-9 followed by '.' followed by 0-9 followed by 0-9
+                if(c == 'E') {
+                    increaseBuffer(twinBuffer);
+                    dfaState = 26;
+                }
+
+                else {
+                    retract(twinBuffer);
+                    dfaState = 30;
+                }
+                break;
+            
+            case 26: // read 0-9 followed by '.' followed by 0-9 followed by 0-9 followed by 'E'
+                if(c == '+' || c == '-') {
+                    increaseBuffer(twinBuffer);
+                    dfaState = 27;
+                }
+                else if(c >= '0' && c <= '9') {
+                    increaseBuffer(twinBuffer);
+                    dfaState = 28;
+                }
+                break;
+            
+            case 27: // read 0-9 followed by '.' followed by 0-9 followed by 0-9 followed by 'E' followed by '+' or '-'
+                if(c >= '0' && c <= '9') {
+                    increaseBuffer(twinBuffer);
+                    dfaState = 28;
+                }
+                break;
+
+            case 28: // read 0-9 followed by '.' followed by 0-9 followed by 0-9 followed by 'E' followed by '+' or '-' followed by 0-9
+                if(c >= '0' && c <= '9') {
+                    increaseBuffer(twinBuffer);
+                    dfaState = 29;
+                }
+                break;
+
+            case 29:
+                retract(twinBuffer);
+                dfaState = 30;
+                break;
+
+            case 30: // read other
+                dfaState = 0;
+                twinBuffer->currPtr = twinBuffer->fwdPtr;
+                return createToken(TK_NUM, lexeme, currLineNum);
+            
+            case 31: // read '#'
+                if(c >= 'a' && c <= 'z') {
+                    increaseBuffer(twinBuffer);
+                    dfaState = 32;
+                }
+                break;
+            
+            case 32: // read '#' followed by a-z
+                if(c >= 'a' && c <= 'z') {
+                    increaseBuffer(twinBuffer);
+                    dfaState = 32;
+                }
+                else {
+                    retract(twinBuffer);
+                    dfaState = 33;
+                }
+                break;
+
+            case 33: // read other 
+                dfaState = 0;
+                twinBuffer->currPtr = twinBuffer->fwdPtr;
+                return createToken(TK_RUID, lexeme, currLineNum);
+            
+            case 34: // read '_'
+                if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+                    increaseBuffer(twinBuffer);
+                    dfaState = 35;
+                }
+                break;
+            
+            case 35: // read '_' followed by a-z or A-Z
+                if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+                    increaseBuffer(twinBuffer);
+                    dfaState = 35;
+                }
+                else if(c >= '0' && c <= '9') {
+                    increaseBuffer(twinBuffer);
+                    dfaState = 36;
+                }
+                else {
+                    retract(twinBuffer);
+                    dfaState = 37;
+                }
+                break;
+            
+            case 36: // read '_' followed by a-z or A-Z followed by 0-9
+                if(c >= '0' && c <= '9') {
+                    increaseBuffer(twinBuffer);
+                    dfaState = 36;
+                }
+                else {
+                    retract(twinBuffer);
+                    dfaState = 37;
+                }
+                break;
+            
+            case 37: // read other
+                dfaState = 0;
+                twinBuffer->currPtr = twinBuffer->fwdPtr;
+                return createToken(TK_FUNID, lexeme, currLineNum);
+
+            case 38: // read '>'
+                if(c == '=') {
+                    increaseBuffer(twinBuffer);
+                    dfaState = 40;
+                }
+                else {
+                    retract(twinBuffer);
+                    dfaState = 39;
+                }
+                break;
+            
+            case 39: // read other
+                dfaState = 0;
+                twinBuffer->currPtr = twinBuffer->fwdPtr;
+                return createToken(TK_GT, ">", currLineNum);
+
+            case 40: // read '>='
+                dfaState = 0;
+                twinBuffer->currPtr = twinBuffer->fwdPtr;
+                return createToken(TK_GE, ">=", currLineNum);
+        }
 }
 
 
