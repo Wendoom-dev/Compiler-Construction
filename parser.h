ec6438808f27ab74e49f39de29cc643198b8b5f4
SanyamJain2704 <122691454+SanyamJain2704@users.noreply.github.com>
2026-03-01 16:05:26 +0530

    Refactor parser.h to update function signatures

--- parser.h
+++ parser.h
@@ -3,16 +3,16 @@
 
 #include "parserDef.h"
 
+void loadGrammarFromFile(Grammar* g, const char* filename);
+void computeFirst(Grammar* g);
+void computeFollow(Grammar* g, int startSymbol);
+void buildParseTable(ParseTable* pt, Grammar* g);
+
 void initProduction(Production* p);
 void addSymbol(Production* p,int sym);
 
 void initGrammar(Grammar* g);
 void addProductionToGrammar(Grammar* g,int nt,Production p);
-
-void loadGrammarFromFile(Grammar* g, const char* filename);
-void computeFirst(Grammar* g);
-void computeFollow(Grammar* g, int startNT);
-void buildParseTable(ParseTable* pt, Grammar* g);
 
 void setAdd(Set* s,int t);
 bool setContains(Set s,int t);
@@ -23,7 +23,7 @@ void setSynch(ParseTable* pt,int nt,int t);
 
 TreeNode* createNode(int symbol);
 void addChild(TreeNode* p,TreeNode* c);
-void printTree(TreeNode* n,int depth);
+void printTree(TreeNode* n, int depth, FILE* fp);
 
 TreeNode* parse(ParseTable* pt,
                 Grammar* g,
